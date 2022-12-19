c02203efe65c1224cfddabbac6fa897db7fd650a
unknown <dullstart.yang.com>
2022-12-20 02:47:30 +0800

    practise__

--- practise_db.php
+++ practise_db.php
@@ -9,9 +9,9 @@ function dd($array)
     echo "</pre>";
 }
 $Student = new DB("students");
-$stu=$Student->all();
-// $Student->all(" where `graduate_at`=4"," order by id desc");
-// $stu = $Student->all(['dept'=>2,'graduate_at'=>2]," order by id desc");
+// $stu=$Student->all();
+// $stu = $Student->all(" where `graduate_at`=4", " order by id desc");
+$stu =$Student->all(['dept'=>2,'graduate_at'=>2]," order by id desc");
 dd($stu);
 // class DB
 // {
@@ -20,19 +20,18 @@ dd($stu);
 //     protected $pdo;
 //     function __construct($table)
 //     {
-//         $this->pdo = new PDO($this->dsn,'root', '');
+//         $this->pdo = new PDO($this->dsn,'root','');
 //         $this->table = $table;
 //     }
 //     function all(...$args)
 //     {
 //         $sql = "SELECT * FROM $this->table";
-//         if (isset($args[0])) {
+//         if(isset($args[0]) {
 //             if (is_array($args[0])) {
 //                 foreach ($args[0] as $key => $value) {
 //                     $tmp[] = "`$key`='$value'";
 //                 }
-
-//                 $sql .=  " WHERE " . join(" && ", $tmp);
+//                 $sql .= " WHERE " . join(" && ", $tmp);
 //             } else {
 //                 $sql .= $args[0];
 //             }
@@ -47,17 +46,34 @@ dd($stu);
 class DB
 {
     protected $table;
-    protected $dsn="mysql:host=localhost;charset=utf8;dbname=school";
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
     protected $pdo;
     function __construct($table)
     {
-        $this->pdo=new PDO($this->dsn,'root','');
-        $this->table=$table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+        $this->table = $table;
     }
-    function all(...$args){
-        $sql="select * from ";
+    function all(...$args)
+    {
+        $sql = "select * from $this->table";
         if (isset($args[0])) {
-            # code...
+            if (is_array($args[0])) {
+                foreach ($args[0] as $key => $value) {
+                    $tmp[] = "`$key`='$value'";
+                }
+                $sql .= " where " . join(" && ", $tmp);
+            } else {
+                $sql .= $args[0];
+            }
         }
+        if (isset($args[1])) {
+            $sql .= $args[1];
+        }
+        echo $sql;
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
+
+
+
+
 }
