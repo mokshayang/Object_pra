1a4a3f127480370656a3aadc09e17e1ffb43eda1
unknown <dullstart.yang.com>
2022-12-21 00:51:23 +0800

    practise__

--- practise_db.php
+++ practise_db.php
@@ -11,8 +11,8 @@ function dd($array)
 $Student = new DB("students");
 // $stu=$Student->all();
 // $stu = $Student->all(" where `graduate_at`=4", " order by id desc");
-$stu =$Student->all(['dept'=>2,'graduate_at'=>2]," order by id desc");
-dd($stu);
+// $stu =$Student->all(['dept'=>2,'graduate_at'=>2]," order by id desc");
+// dd($stu);
 // class DB
 // {
 //     protected $table;
@@ -46,34 +46,15 @@ dd($stu);
 class DB
 {
     protected $table;
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=school";
     protected $pdo;
     function __construct($table)
     {
-        $this->pdo = new PDO($this->dsn, 'root', '');
-        $this->table = $table;
+        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table=$table;
     }
-    function all(...$args)
-    {
-        $sql = "select * from $this->table";
-        if (isset($args[0])) {
-            if (is_array($args[0])) {
-                foreach ($args[0] as $key => $value) {
-                    $tmp[] = "`$key`='$value'";
-                }
-                $sql .= " where " . join(" && ", $tmp);
-            } else {
-                $sql .= $args[0];
-            }
-        }
-        if (isset($args[1])) {
-            $sql .= $args[1];
-        }
-        echo $sql;
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-
+  
+   
 
 
 }
