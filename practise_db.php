61029ba225f15b8c97291e56281ad3dc750d62f0
unknown <dullstart.yang.com>
2022-12-21 21:34:04 +0800

    all()_OOP

--- practise_db.php
+++ practise_db.php
@@ -10,9 +10,9 @@ function dd($array)
 }
 $Student = new DB("students");
 // $stu=$Student->all();
-// $stu = $Student->all(" where `graduate_at`=4", " order by id desc");
+$stu = $Student->all(" where `graduate_at`=4", " order by id desc");
 // $stu =$Student->all(['dept'=>2,'graduate_at'=>2]," order by id desc");
-// dd($stu);
+dd($stu);
 // class DB
 // {
 //     protected $table;
@@ -46,35 +46,31 @@ $Student = new DB("students");
 class DB
 {
     protected $table;
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=school";
     protected $pdo;
     function __construct($table)
     {
-        $this->pdo = new PDO($this->dsn, 'root', '');
-        $this->table = $table;
+        $this->pdo=new PDO($this->dsn,'root','');
+        $this->table=$table;
     }
-    //UPDATE TABLE SET ` `=' ',` `=' '.... WHERE ``=''
-    //INSERT INTO TABLE (``,``....) VALUES ('',''....)
-    function save($array){
-        if(isset($array['id'])){
-            foreach ($array as $key => $value) {
-                if($key!='id'){
-                    $tmp[]="`$key`='$value'";
+    function all(...$args){
+        $sql="select * from $this->table";
+        if(isset($args[0])){
+            if(is_array($args[0])){
+                foreach ($args[0] as $key => $value) {
+                    $tmp[]="$key``='$value'";
                 }
+                $sql .= " where " . join( " && ",$tmp);
+            }else{
+                $sql .= $args[0];
             }
-            $sql = "update $this->table set";
-            $sql .=join(",",$tmp);
-            $sql .= " where `id`='{$array['id']}'"; 
-        }else{
-            $cols=array_keys($array);
-            $sql = "insert into $this->table (`" . join("`,`",$cols) . "`)
-                                    values ('" . join("','",$array) . "')";
+        }
+        if(is_array($args[1])){
+            $sql .= $args[1];
         }
         echo $sql;
-        // return $this->pdo->exec($sql);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
 
 }
-$save=$Student->save(['name'=>'成功了喔','tel'=>'0975-398680','id'=>'488']);
-// dd($save);
